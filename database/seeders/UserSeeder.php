359e90d0cc6b14e87854eba9f11e74d6784ef83e
teddykatami <aarebateddyhidden00@gmail.com>
2023-07-03 16:29:23 +0300

    first commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -14,6 +13,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+
     }
 }
